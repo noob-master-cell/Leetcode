c220d06ad616e55db17cf2d1c99fea67e6ecc26f
Dheeraj  Karwasra <dheerajkarwasra42@gmail.com>
2022-10-13 15:15:28 +0530

    Time: 472 ms (14.29%), Space: 58.9 MB (31.75%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,51 +1,68 @@
 class Solution {
 public:
-    bool dfsCheck(int node, vector<vector<int>>& graph, vector<int> &vis, vector<int> &dfsvis, vector<int> &safe)
-    {
-        vis[node]=1;
-        dfsvis[node]=1;
-        safe[node]=0;
-        for(auto it : graph[node])
-        {
-            if(!vis[it])
-            {
-                if(dfsCheck(it, graph, vis, dfsvis, safe))
-                {
-                    safe[node]=0;
-                    return true;
-                }
-            }
-            else if(dfsvis[it])
-            {
-                safe[node]=0;
-                return true;
-            }
-        }
-        dfsvis[node]=0;
-        safe[node]=1;
-        return false;
-    }
+    
+    // bool dfsCheck(int node, vector<vector<int>>& graph, vector<int> &vis, vector<int> &dfsvis, vector<int> &safe)
+    // {
+    //     vis[node]=1;
+    //     dfsvis[node]=1;
+    //     safe[node]=0;
+    //     for(auto it : graph[node])
+    //     {
+    //         if(!vis[it])
+    //         {
+    //             if(dfsCheck(it, graph, vis, dfsvis, safe))
+    //             {
+    //                 safe[node]=0;
+    //                 return true;
+    //             }
+    //         }
+    //         else if(dfsvis[it])
+    //         {
+    //             safe[node]=0;
+    //             return true;
+    //         }
+    //     }
+    //     dfsvis[node]=0;
+    //     safe[node]=1;
+    //     return false;
+    // }
+    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) 
     {
         int n = graph.size();
-        int m = graph[0].size();
-        vector<int> vis(n,0);
-        vector<int> dfsVis(n,0);
-        vector<int> safe(n, 0);
+        vector<int> adjRev[n];
+        vector<int> indegree(n,0);
         vector<int> ans;
+        
         for(int i=0; i<n; i++)
         {
-            if(!vis[i])
+            for(auto it : graph[i])
             {
-                dfsCheck(i, graph, vis, dfsVis, safe);
+                adjRev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0; i<n; i++)
+        {
+            if(indegree[i]==0)
+                q.push(i);
+        }
+        
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adjRev[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                    q.push(it);
             }
         }
     
-        for(int i=0; i<n; i++)
-        {
-            if(safe[i]==1)
-                ans.push_back(i);
-        }
+        sort(ans.begin(), ans.end());
         return ans; 
     }
 };
