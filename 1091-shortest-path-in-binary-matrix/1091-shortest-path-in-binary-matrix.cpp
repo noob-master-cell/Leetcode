d86c0cb58eb7587878ec964ac821ce806b4c292a
Dheeraj  Karwasra <dheerajkarwasra42@gmail.com>
2022-10-17 19:51:11 +0530

    Time: 293 ms (20.90%), Space: 31.5 MB (27.93%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -10,20 +10,21 @@ public:
     
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) 
     {
-        if(grid[0][0] == 1)
+        if(grid[0][0] == 1) // if starting point is 1 then it means that it is not possible to movie forwart in any direction 
             return -1;
         
         int n = grid.size(); // given that grid is N x N
         
-        set<pair<int, pair<int, int>>> s; // {distance {x-cordinate, y-cordinate}}
+        set<pair<int, pair<int, int>>> s; // set is used to eliminate unnecessary iterations {distance {x-cordinate, y-cordinate}}
         
         vector<vector<int>> dist(n, vector<int>(n, INT_MAX)); // 2-D vector for storing distance of each cordinate initally having infinite distance
         
         dist[0][0] = 1;
         s.insert({1, {0, 0}});
         
-        int delRow[] = {-1,-1,-1,0,0,1,1,1};
-        int delCol[] = {-1,0,1,-1,1,-1,0,1};
+        // for generating 8 directions at each coordinate (left, right, up, down, diagonally)
+        int delRow[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int delCol[] = {-1, 0, 1, -1, 1, -1, 0, 1};
         
         while(!s.empty())
         {
@@ -35,23 +36,24 @@ public:
             
             for(int i=0; i<8; i++)
             {
+                // moving in each direction (left, right, up, down, diagonally)
                 int row = r + delRow[i];
                 int col = c + delCol[i];
                 
-                if(isPossible(row, col, n, grid) && distance + 1 < dist[row][col])
+                if(isPossible(row, col, n, grid) && distance + 1 < dist[row][col]) // checking if it is clear path or not and new distance is less than previous distance
                 {
-                    if(dist[row][col] != INT_MAX)
+                    if(dist[row][col] != INT_MAX) // if set contains distance for row, col remove it from set
                         s.erase({distance, {row, col}});
                     
                     dist[row][col] = distance + 1;
-                    s.insert({distance + 1, {row, col}});
+                    s.insert({distance + 1, {row, col}}); // insert new shortest distance for rowset 
                 }
             }
         }
         
-        if(dist[n-1][n-1] == INT_MAX)
+        if(dist[n-1][n-1] == INT_MAX) // if last [n-1][n-1] position is still infinity it means there is no  clear path so we return -1
             return -1;
         
-        return dist[n-1][n-1];
+        return dist[n-1][n-1]; 
     }
 };
